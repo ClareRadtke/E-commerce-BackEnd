872b6bdabb27f2c6214d94e86986c18346563d49
Clare Radtke <clare.radtke@outlook.com>
2021-05-23 14:26:20 +1000

    Initial Commit

---
+++ Develop/db/schema.sql
@@ -0,0 +1,9 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
+USE ecommerce_db;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 't25R9pf7Q486sA3S8bj2HDCtTYYjbbxQ';
+
